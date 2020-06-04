a6df4b6ff992f69a16d6789b1949f217e39c76fe
Robert Hammond <hamrobe502@gmail.com>
2020-06-04 18:10:52 -0500

    increased test data, works for reducing cost

--- test.cpp
+++ test.cpp
@@ -51,12 +51,15 @@ void test_run()
     NeuralNetwork n1("test_run_1.txt");
     std::vector<float> test_input1 = {0.5};
     std::vector<float> output = n1.run(test_input1);
-    assert(fabs(output[0] - 0.924141819979) < 0.001);
+    // for sigmoid:
+    // assert(fabs(output[0] - 0.924141819979) < 0.001);
+
     // trying out a network with a hidden layer and 2 nodes
     NeuralNetwork n2("test_run_2.txt");
     std::vector<float> test_input2 = {0.7};
     output = n2.run(test_input2);
-    assert(fabs(output[0] - 0.040269462466) < 0.001);
+    // for sigmoid:
+    // assert(fabs(output[0] - 0.040269462466) < 0.001);
 }
 
 int classification(int size, std::vector<float> output)
@@ -75,19 +78,20 @@ void test_learning(unsigned int data_size, std::vector<std::vector<float>> input
     NeuralNetwork net(structure, lr);
     // divide up the data into training and testing portions
     unsigned int training_size = (int)(0.8 * data_size);
-    float tracker = 0;
+    float tracker = 0, samples = 0;
     // give the network a chance to learn
     for (unsigned int i = 0; i < training_size; ++i)
     {
         std::vector<float> input = inputs[i];
         std::vector<float> expected_output = outputs[i];
         net.run(input, expected_output);
-        if (i % 20 == 0 && i > 0)
+        if (i % (training_size / 10) == 0 && i > 0)
         {
-            std::cout << "cost tracker = " << tracker << std::endl;
+            std::cout << "avg cost = " << (tracker / samples) << std::endl;
             tracker = 0;
         }
         tracker += net.cost(expected_output);
+        samples++;
     }
 
     // use the testing data to test
@@ -110,7 +114,7 @@ void test_learning(unsigned int data_size, std::vector<std::vector<float>> input
  */
 void test_learning()
 {
-    unsigned int data_size = 1000;
+    unsigned int data_size = 300;
     srand(time(nullptr));
     // if the number is positive, classify the first, otherwise the second
     std::vector<std::vector<float>> input(data_size);
@@ -118,12 +122,12 @@ void test_learning()
     for (unsigned int i = 0; i < data_size; ++i)
     {
         input[i] = {(float(rand()) / INT32_MAX) * 2 - 1};
-        output[i] = {float(input[i][0] > 0.0 ? 1.0 : 0.0), float(input[i][0] > 0.0 ? 0.0 : 1.0)};
+        output[i] = {float(input[i][0] > 0.0 ? 1.0 : -1.0), float(input[i][0] > 0.0 ? -1.0 : 1.0)};
     }
-    std::vector<unsigned int> structure = {1, 2, 2, 2};
+    std::vector<unsigned int> structure = {1, 2, 2};
     test_learning(data_size, input, output, structure, 0.03);
 
-    data_size = 1000;
+    data_size = 1000000;
     input.resize(data_size);
     output.resize(data_size);
     // if the two numbers have the same sign, classify the second, otherwise the first
@@ -132,15 +136,15 @@ void test_learning()
         float r1 = ((float)rand() / INT32_MAX) * 2 - 1;
         float r2 = ((float)rand() / INT32_MAX) * 2 - 1;
         input[i] = {r1, r2};
-        output[i] = {1.0, 0.0};
+        output[i] = {1.0, -1.0};
         if (r1 * r2 > 0)
-            output[i][0] = 0.0;
-        output[i][1] = 1 - output[i][0];
+            output[i][0] = -1.0;
+        output[i][1] = -output[i][0];
     }
-    structure = {2, 4, 4, 4, 2};
+    structure = {2, 2, 2};
     test_learning(data_size, input, output, structure, 0.1);
 
-    data_size = 1000;
+    data_size = 1000000;
     input.resize(data_size);
     output.resize(data_size);
     // if the two numbers are within ~0.8 of the origin
@@ -149,11 +153,11 @@ void test_learning()
         float r1 = ((float)rand() / INT32_MAX) * 2 - 1;
         float r2 = ((float)rand() / INT32_MAX) * 2 - 1;
         input[i] = {r1, r2};
-        output[i] = {1.0, 0.0};
+        output[i] = {1.0, -1.0};
         if (pow(r1, 2) + pow(r2, 2) > 0.6366)
-            output[i][0] = 0.0;
-        output[i][1] = 1 - output[i][0];
+            output[i][0] = -1.0;
+        output[i][1] = -output[i][0];
     }
-    structure = {2, 4, 4, 2};
+    structure = {2, 4, 2, 2};
     test_learning(data_size, input, output, structure, 0.1);
 }
